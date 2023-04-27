d9c4c61555175e07c95e7c2d8b90f27e814238f7
JohnnyMahoney <khomyk.dima42@gmail.com>
2023-04-27 18:16:39 +0200

    Initial commit

---
+++ app/src/main/java/com/example/finapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.finapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.example.finapp.R
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
