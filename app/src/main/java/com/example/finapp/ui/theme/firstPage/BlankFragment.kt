5d0be244ffa5df51a7c1c3d691f6c589f135ba5f
JohnnyMahoney <khomyk.dima42@gmail.com>
2023-07-15 03:57:25 +0200

    VERSION 1.0

--- app/src/main/java/com/example/finapp/ui/theme/firstPage/BlankFragment.kt
+++ app/src/main/java/com/example/finapp/ui/theme/firstPage/BlankFragment.kt
@@ -6,10 +6,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.finapp.R
+import com.example.finapp.data.model.Category
+import com.example.finapp.data.model.TransactionType
+import com.example.finapp.data.repository.TransactionsRepository
 import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.data.PieData
@@ -25,16 +29,22 @@ class BlankFragment : Fragment() {
     }
 
     private lateinit var viewModel: BlankViewModel
-    lateinit var pieChart: PieChart
-    val entries: ArrayList<PieEntry> = ArrayList()
-    private var isChartDataLoaded = false
+    private lateinit var pieChartOutcome: PieChart
+    private lateinit var pieChartIncome: PieChart
+    private lateinit var categoryTextViews: Array<TextView>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_blank, container, false)
-        pieChart = view.findViewById(R.id.pieChart)
+        pieChartOutcome = view.findViewById(R.id.pieChartOutcome)
+        pieChartIncome = view.findViewById(R.id.pieChartIncome)
+        categoryTextViews = arrayOf(
+            view.findViewById(R.id.textViewRent),
+            view.findViewById(R.id.textViewCar),
+            view.findViewById(R.id.textViewFood)
+        )
         return view
     }
 
@@ -45,10 +55,12 @@ class BlankFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupPieChart()
+        setupPieChart(pieChartOutcome)
+        setupPieChart(pieChartIncome)
         loadChartData()
     }
-    private fun setupPieChart() {
+
+    private fun setupPieChart(pieChart: PieChart) {
         pieChart.setUsePercentValues(true)
         pieChart.description.isEnabled = false
         pieChart.setExtraOffsets(5f, 10f, 5f, 5f)
@@ -70,12 +82,45 @@ class BlankFragment : Fragment() {
     }
 
     private fun loadChartData() {
-        entries.clear()
-        entries.add(PieEntry(70f))
-        entries.add(PieEntry(20f))
-        entries.add(PieEntry(10f))
+        val transactionsRepository = TransactionsRepository()
+        transactionsRepository.getAllTransactions { transactionList ->
+            val outcomeEntries: ArrayList<PieEntry> = ArrayList()
+            val incomeEntries: ArrayList<PieEntry> = ArrayList()
 
-        val dataSet = PieDataSet(entries, "Mobile OS")
+            val outcomeCategoryMap = HashMap<Category, Float>()
+            val incomeCategoryMap = HashMap<Category, Float>()
+
+            for (transaction in transactionList) {
+                val transactionValue = transaction.value.toFloatOrNull()
+                if (transactionValue != null) {
+                    val currentCategoryValue = when (transaction.type) {
+                        TransactionType.outcome, TransactionType.OUTCOME -> outcomeCategoryMap
+                        TransactionType.income, TransactionType.INCOME -> incomeCategoryMap
+                        else -> null
+                    }
+
+                    if (currentCategoryValue != null) {
+                        val currentValue = currentCategoryValue[transaction.category] ?: 0f
+                        currentCategoryValue[transaction.category] = currentValue + transactionValue
+                    }
+                }
+            }
+
+            for ((category, value) in outcomeCategoryMap) {
+                outcomeEntries.add(PieEntry(value, category.name))
+            }
+
+            for ((category, value) in incomeCategoryMap) {
+                incomeEntries.add(PieEntry(value, category.name))
+            }
+
+            loadChart(pieChartOutcome, outcomeEntries)
+            loadChart(pieChartIncome, incomeEntries)
+        }
+    }
+
+    private fun loadChart(pieChart: PieChart, entries: ArrayList<PieEntry>) {
+        val dataSet = PieDataSet(entries, "")
         dataSet.setDrawIcons(false)
         dataSet.sliceSpace = 3f
         dataSet.iconsOffset = MPPointF(0f, 40f)
@@ -85,6 +130,9 @@ class BlankFragment : Fragment() {
         colors.add(ContextCompat.getColor(requireContext(), R.color.purple_200))
         colors.add(ContextCompat.getColor(requireContext(), R.color.yellow))
         colors.add(ContextCompat.getColor(requireContext(), R.color.red))
+        colors.add(ContextCompat.getColor(requireContext(), R.color.grey))
+        // Add more colors if needed for additional categories
+
         dataSet.colors = colors
 
         val data = PieData(dataSet)
@@ -94,9 +142,6 @@ class BlankFragment : Fragment() {
         data.setValueTextColor(Color.WHITE)
         pieChart.data = data
         pieChart.highlightValues(null)
+        pieChart.invalidate()
     }
 }
-
-
-
-
